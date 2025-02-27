ceec1309f3967ed0bbdd0725be0f73d80c49d21e
kkim-2 <kimkailyn247@gmail.com>
2025-02-27 10:29:19 -0500

    Update TempConverter.java

--- TempConverter.java
+++ TempConverter.java
@@ -2,10 +2,11 @@
 import java.util.*;
 public class TempConverter {
     public static void main(String[]args){
-        int fahrenheit;
-        int celsius;
+        double fahrenheit;
+        double celsius;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a temp in fahrenheit: ");
+        fahrenheit = input.nextInt();  
         celsius = 5/9*(fahrenheit - 32);  
         System.out.println("The temperature in celsius is: " + celsius);
     }    
